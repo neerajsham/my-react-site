51767ccd240b1aa5b412982e049ca9009d7fbf85
Neeraj Sharma <neeraj.sham95@gmail.com>
2026-02-09 15:11:59 +0530

    fix css render

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
-    },
+  base: "/",
+  build: {
+    outDir: "dist",
   },
+  plugins: [react(), tailwindcss()],
+});
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
-  assetsInclude: ['**/*.svg', '**/*.csv'],
-})
